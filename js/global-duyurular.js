f2f9b835e0544b1f392e62c187918552c35bdaa4
veriizlemekojenerasyon <veriizlemekojenerasyon@gmail.com>
2026-08-03 12:50:18 +0300

    Update global-duyurular.js

--- js/global-duyurular.js
+++ js/global-duyurular.js
@@ -391,7 +391,7 @@
                 gap: 34px;
                 white-space: nowrap;
                 min-width: max-content;
-                animation: globalTickerScroll 180s linear infinite;
+                animation: globalTickerScroll 120s linear infinite;
             }
 
             .global-announcement-ticker:hover .global-ticker-track {
